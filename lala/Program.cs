3e483a800e04ee359c11b44011a4c466ec587417
212201 <212201@1220-012-22>
2023-11-02 12:09:13 +0300

    под

--- lala/Program.cs
+++ lala/Program.cs
@@ -2,22 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using lala.classes;
+using lala.classes.fabrics;
 
-List<Workers> people = new List<Workers>();
-people.Add(new Doctors("Антон", 37, "КДЦ", 50000));
-people.Add(new Doctors("Арсений", 38, "КДЦ", 55000));
-people.Add(new Doctors("Стас", 40, "КДЦ", 63000));
-people.Add(new Nurses("Оксана", 25, "КДЦ", 28000));
-people.Add(new Nurses("Сергей", 37, "КДЦ", 30000));
-people.Add(new Nurses("Дмитрий", 36, "КДЦ", 32000));
-people.Add(new Administrators("Савина", 26, "КДЦ", 25000));
-people.Add(new Administrators("Павел", 19, "КДЦ", 25000));
+
+Doctors doctor1 = DoctorsFabric.Create("Антон", 37, "КДЦ", 50000);
+Doctors doctor2 = DoctorsFabric.Create("Арсений", 38, "КДЦ", 55000);
+Doctors doctor3 = DoctorsFabric.Create("Стас", 40, "КДЦ", 63000);
+Nurses nurses1 = NursesFabric.Create("Оксана", 25, "КДЦ", 28000);
+Nurses nurses2 = NursesFabric.Create("Сергей", 37, "КДЦ", 30000);
+Nurses nurses3 = NursesFabric.Create("Дмитрий", 36, "КДЦ", 32000);
+Administrators administrators1 = AdministratorsFabric.Create("Савина", 26, "КДЦ", 25000);
+Administrators administrators2 = AdministratorsFabric.Create("Павел", 19, "КДЦ", 25000);
+List<Workers> people = new List<Workers> { doctor1, doctor2, doctor3, nurses1, nurses2, nurses3, administrators1, administrators2 };
 BubbleSortByZp(people);
 void ListOutput()
 {
-    foreach (Workers people in people)
+    foreach (Workers person in people)
     {
-        Console.WriteLine(people.PrintP());
+        person.PrintP();
     }
 }
 
@@ -26,7 +28,7 @@ void BubbleSortByZp(List<Workers> people)
 {
     for (int j = 0; j < people.Count; j++)
     {
-        for (int i = 0; i < people.Count; i++)
+        for (int i = 0; i < people.Count-1; i++)
         {
             if (people[i].Zp > people[i + 1].Zp)
             {
